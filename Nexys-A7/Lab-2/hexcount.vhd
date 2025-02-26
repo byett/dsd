1276344608b8d832a40fbd7896d35a2a93a73a92
byett <bernard.h.yett@vanderbilt.edu>
2025-02-26 09:48:39 -0500

    Update hexcount.vhd

--- Nexys-A7/Lab-2/hexcount.vhd
+++ Nexys-A7/Lab-2/hexcount.vhd
@@ -17,7 +17,7 @@ ARCHITECTURE Behavioral OF hexcount IS
 		PORT (
 			clk : IN STD_LOGIC;
 			count : OUT STD_LOGIC_VECTOR (15 DOWNTO 0);
-			mpx : OUT STD_LOGIC_VECTOR (2 DOWNTO 0)
+			dig : OUT STD_LOGIC_VECTOR (2 DOWNTO 0)
 		);
 	END COMPONENT;
 
@@ -31,18 +31,18 @@ ARCHITECTURE Behavioral OF hexcount IS
 	END COMPONENT;
 
 	SIGNAL S : STD_LOGIC_VECTOR (15 DOWNTO 0); -- Connect C1 and L1 for values of 4 digits
-	SIGNAL md : STD_LOGIC_VECTOR (2 DOWNTO 0); -- mpx selects displays
+	SIGNAL dig : STD_LOGIC_VECTOR (2 DOWNTO 0); -- dig selects displays
 	SIGNAL display : STD_LOGIC_VECTOR (3 DOWNTO 0); -- Send digit for only one display to leddec
 
 BEGIN
 	C1 : counter
-	PORT MAP(clk => clk_100MHz, count => S, mpx => md);
+	PORT MAP(clk => clk_100MHz, count => S, dig => dig);
 	L1 : leddec
-	PORT MAP(dig => md, data => display, anode => anode, seg => seg);
+	PORT MAP(dig => dig, data => display, anode => anode, seg => seg);
 
-	display <= S(3 DOWNTO 0) WHEN md = "000" ELSE
-	           S(7 DOWNTO 4) WHEN md = "001" ELSE
-	           S(11 DOWNTO 8) WHEN md = "010" ELSE
+	display <= S(3 DOWNTO 0) WHEN dig = "000" ELSE
+	           S(7 DOWNTO 4) WHEN dig = "001" ELSE
+	           S(11 DOWNTO 8) WHEN dig = "010" ELSE
 	           S(15 DOWNTO 12);
 
 END Behavioral;
