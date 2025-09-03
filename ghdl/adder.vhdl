d4faffc949be108cfdde4fac42645ae6ff1eb9d4
byett <bernard.h.yett@vanderbilt.edu>
2025-09-03 10:39:42 -0400

    Update adder.vhdl

--- ghdl/adder.vhdl
+++ ghdl/adder.vhdl
@@ -1,15 +1,15 @@
 --  https://ghdl.readthedocs.io/en/stable/using/QuickStartGuide.html
 entity adder is
-  -- `i0`, `i1`, and the carry-in `ci` are inputs of the adder.
+  -- `a`, `b`, and the carry-in `ci` are inputs of the adder.
   -- `s` is the sum output, `co` is the carry-out.
-  port (i0, i1 : in bit; ci : in bit; s : out bit; co : out bit);
+  port (ci : in bit; a, b : in bit; s : out bit; co : out bit);
 end adder;
 
 architecture rtl of adder is
 begin
   --  This full-adder architecture contains two concurrent assignments.
   --  Compute the sum.
-  s <= i0 xor i1 xor ci;
+  s <= a xor b xor ci;
   --  Compute the carry.
-  co <= (i0 and i1) or (i0 and ci) or (i1 and ci);
+  co <= (a and b) or (a and ci) or (b and ci);
 end rtl;
